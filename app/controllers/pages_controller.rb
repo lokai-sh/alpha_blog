79b9498f6d55acef47baec0729dde96faf5fb311
lokai-sh <tonja@teolis.ca>
2021-07-12 21:53:35 -0700

    Initial Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+
+  def home #This method definition will call the pages/home.html.erb file and display the contents (which can be html with ruby embedded)
+  end
+
+  def about #This methos definition will call the pages/about.html.erb file and display the contents 
+  end
+
+end
