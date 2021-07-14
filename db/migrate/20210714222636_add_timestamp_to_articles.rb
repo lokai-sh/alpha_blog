4628e45b6afce24ce5776a8396364de3ce67d0fc
lokai-sh <tonja@teolis.ca>
2021-07-14 15:31:34 -0700

    Adding Articles Table and fields

---
+++ db/migrate/20210714222636_add_timestamp_to_articles.rb
@@ -0,0 +1,7 @@
+class AddTimestampToArticles < ActiveRecord::Migration[6.1]
+  def change
+    #add_column :articles, :created_at, :datetime
+    #add_column :articles, :updated_at, :datetime
+    #commenting this out as my table came with these columns by default but wanted to go through the exercise
+  end
+end
