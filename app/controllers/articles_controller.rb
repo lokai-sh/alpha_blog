671b0c48b902b987cc42eaff98fc150a5b32b2b1
lokai-sh <tonja@teolis.ca>
2021-07-26 20:06:11 -0700

    Add Articles Index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,10 @@
 class ArticlesController < ApplicationController
   def show #This method definition will call the articles/show.html.erb file and display the contents (which can be html with ruby embedded)
     #Turn this into an instance variable so it is available to the view
-    byebug
+    #byebug
     @article = Article.find(params[:id])
+  end
+  def index 
+    @articles = Article.all
   end
 end
